5c70769d9a4a79d4320faf64c5e02b2cd9e0771e
Jake Lee <jaketl2002@gmail.com>
2019-09-05 11:59:56 -0500

    story output activity made

--- app/src/main/java/com/example/madlibsapp/MainActivity.java
+++ app/src/main/java/com/example/madlibsapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.madlibsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void storyOne(View v)
+    {
+        Intent intent = new Intent(this, StoryOne.class);
+
+        startActivity(intent);
+    }
+    public void storyTwo(View v)
+    {
+        Intent intent = new Intent(this, StoryOne.class);
+
+        startActivity(intent);
+    }
+    public void storyThree(View v)
+    {
+        Intent intent = new Intent(this, StoryOne.class);
+
+
+        startActivity(intent);
     }
 }
